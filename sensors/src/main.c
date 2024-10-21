e7390933018f06302e22a68e5ba674bc73f47e93
lain <lain@hacktheinter.net>
2024-10-21 15:57:40 -0700

    add sensor code

---
+++ sensors/src/main.c
@@ -0,0 +1,81 @@
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/i2c.h>
+
+#include "sensor.h"
+
+static const struct device* dev_i2c = DEVICE_DT_GET(DT_NODELABEL(arduino_i2c));
+
+static const struct sensor_t* sensors[] = {
+    &sensor_sen5x,
+    &sensor_sunrise,
+    &sensor_lps22hh,
+};
+
+
+void sensor_read_loop(void) {
+    bool is_ready = false;
+    printf("Reading sensors in a loop...\n");
+    while (true) {
+        ARRAY_FOR_EACH(sensors, i) {
+            const struct sensor_t* const s = sensors[i];
+            if (s->is_faulted()) {
+                printf("[sensor:%s] Faulted, re-initializing all sensors\n", s->name);
+                return;
+            } else if (s->is_ready(&is_ready) != 0) {
+                printf("[sensor:%s] Ready check failed\n", s->name);
+            } else if (!is_ready) {
+                /* not ready, take no action */
+            } else if (s->read() != 0) {
+                printf("[sensor:%s] Read failed\n", s->name);
+            } else {
+                printf("[sensor:%s] success!\n", s->name);
+            }
+        }
+        k_msleep(5000);
+    }
+    printf("Sensor read loop exited.\n");
+}
+
+
+int main(void)
+{
+    printf("henlo, friendo!\n");
+
+    while (!device_is_ready(dev_i2c)) {
+        printf("waiting for i2c to be ready...\n");
+        k_msleep(100);
+    }
+
+    printf("i2c bus scan:\n");
+    printf("    0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
+    for (uint8_t y = 0; y <= 7; y++) {
+        printf("%02x", y<<4);
+        for (uint8_t x = 0; x <= 0xf; x++) {
+            uint8_t addr = (y<<4) | x;
+            uint8_t buf = 0;
+            if (addr < 0x08 || addr > 0x77) {
+                printf("   ");
+            } else if (i2c_read(dev_i2c, &buf, 0, addr) == 0) {
+                printf(" %02x", addr);
+            } else {
+                printf(" --");
+            }
+        }
+        printf("\n");
+    }
+
+    while (true) {
+        printf("Initializing sensors...\n");
+        ARRAY_FOR_EACH(sensors, i) {
+            const struct sensor_t* const s = sensors[i];
+            if (s->init() != 0) {
+                printf("[sensor:%s] Init failed :<\n", s->name);
+            }
+        }
+        sensor_read_loop();
+    }
+
+    return 0;
+}
+
+//eof
