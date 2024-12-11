7c50e946504b5d599a0ffed9737e6cddf3d186f2
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2024-12-11 17:16:17 -0600

    Added meshes folder

---
+++ zephyrapp/game/meshes/mesh_assets.h
@@ -0,0 +1,14 @@
+// Generated from meshes/meshes.json
+
+#pragma once
+
+typedef struct CAT_mesh
+{
+	const char* path;
+	float* verts;
+	int n_verts;
+	int* faces;
+	int n_faces;
+} CAT_mesh;
+
+extern CAT_mesh eth_mesh;
