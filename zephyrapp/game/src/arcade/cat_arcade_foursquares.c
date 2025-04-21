eaf07443dd2d60e85562617d9380f8e99ba13f4d
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2025-04-21 13:51:24 +0900

    First draft of wall kick system

--- zephyrapp/game/src/arcade/cat_arcade_foursquares.c
+++ zephyrapp/game/src/arcade/cat_arcade_foursquares.c
@@ -24,15 +24,17 @@ typedef enum
 
 typedef enum
 {
+	CAT_FOURSQUARES_NORTH,
+	CAT_FOURSQUARES_EAST,
+	CAT_FOURSQUARES_SOUTH,
+	CAT_FOURSQUARES_WEST
+} CAT_foursquares_orientation;
+
+typedef enum
+{
 	CAT_FOURSQUARES_CW,
 	CAT_FOURSQUARES_CCW
 } CAT_foursquares_rotation;
-
-typedef struct
-{
-	CAT_foursquares_type type;
-	CAT_ivec2 position;
-} CAT_foursquares_piece;
 
 typedef struct
 {
@@ -51,15 +53,70 @@ static uint16_t piece_colours[7] =
 	0xF0F0,
 };
 
+static CAT_ivec2 jlstz_kick_offset_bases[5] =
+{
+	{0, 0},
+	{-1, 0},
+	{-1, 1},
+	{0, -2},
+	{-1, -2}
+};
+
+static CAT_ivec2 jlstz_kick_offset_signs[5] =
+{
+	{1, 1},
+	{-1, -1},
+	{1, 1},
+	{1, -1},
+	{-1, 1}
+};
+
+static CAT_ivec2 i_kick_offsets[4][5] =
+{
+	{
+		{0, 0},
+		{-2, 0},
+		{1, 0},
+		{-2, -1},
+		{1, 2}
+	},
+	{
+		{0, 0},
+		{-1, 0},
+		{2, 0},
+		{-1, 2},
+		{2, 1}
+	},
+	{
+		{0, 0},
+		{2, 0},
+		{-1, 0},
+		{2, 1},
+		{-1, 2}
+	},
+	{
+		{0, 0},
+		{1, 0},
+		{-2, 0},
+		{1, -2},
+		{-2, 1}
+	}
+};
+
 static CAT_foursquares_cell cells[CAT_FOURSQUARES_GRID_HEIGHT][CAT_FOURSQUARES_GRID_WIDTH];
-static CAT_foursquares_piece piece;
 
-static uint8_t collision_matrix[4][4];
-static uint8_t collision_w = 4;
-static uint8_t collision_h = 4;
-static uint8_t rotation_buffer_matrix[4][4];
+CAT_foursquares_type piece_type;
+CAT_ivec2 piece_position;
+CAT_foursquares_orientation piece_orientation;
+static uint8_t collider[4][4];
+static uint8_t collider_w = 4;
+static uint8_t collider_h = 4;
 
-void configure_collision_matrix
+CAT_ivec2 piece_position_buffer;
+CAT_foursquares_orientation piece_orientation_buffer;
+static uint8_t collider_buffer[4][4];
+
+void configure_collider
 (
 	uint8_t m00, uint8_t m01, uint8_t m02, uint8_t m03,
 	uint8_t m10, uint8_t m11, uint8_t m12, uint8_t m13,
@@ -68,25 +125,23 @@ void configure_collision_matrix
 	uint8_t w, uint8_t h
 )
 {
-	collision_matrix[0][0] = m00; collision_matrix[0][1] = m01; collision_matrix[0][2] = m02; collision_matrix[0][3] = m03;
-	collision_matrix[1][0] = m10; collision_matrix[1][1] = m11; collision_matrix[1][2] = m12; collision_matrix[1][3] = m13;
-	collision_matrix[2][0] = m20; collision_matrix[2][1] = m21; collision_matrix[2][2] = m22; collision_matrix[2][3] = m23;
-	collision_matrix[3][0] = m30; collision_matrix[3][1] = m31; collision_matrix[3][2] = m32; collision_matrix[3][3] = m33;
-	collision_w = w; collision_h = h;
+	collider[0][0] = m00; collider[0][1] = m01; collider[0][2] = m02; collider[0][3] = m03;
+	collider[1][0] = m10; collider[1][1] = m11; collider[1][2] = m12; collider[1][3] = m13;
+	collider[2][0] = m20; collider[2][1] = m21; collider[2][2] = m22; collider[2][3] = m23;
+	collider[3][0] = m30; collider[3][1] = m31; collider[3][2] = m32; collider[3][3] = m33;
+	collider_w = w; collider_h = h;
 }
 
 void spawn_piece(CAT_foursquares_type type, CAT_ivec2 position)
 {
-	piece = (CAT_foursquares_piece)
-	{
-		.type = type,
-		.position = position
-	};
+	piece_type = type;
+	piece_orientation = CAT_FOURSQUARES_NORTH;
+	piece_position = position;
 
 	switch (type)
 	{
 		case CAT_FOURSQUARES_I:
-			configure_collision_matrix
+			configure_collider
 			(
 				0, 0, 0, 0,
 				1, 1, 1, 1,
@@ -97,7 +152,7 @@ void spawn_piece(CAT_foursquares_type type, CAT_ivec2 position)
 		break;
 
 		case CAT_FOURSQUARES_J:
-			configure_collision_matrix
+			configure_collider
 			(
 				0, 0, 1, 0,
 				1, 1, 1, 0,
@@ -108,7 +163,7 @@ void spawn_piece(CAT_foursquares_type type, CAT_ivec2 position)
 		break;
 		
 		case CAT_FOURSQUARES_L:
-			configure_collision_matrix
+			configure_collider
 			(
 				1, 0, 0, 0,
 				1, 1, 1, 0,
@@ -119,7 +174,7 @@ void spawn_piece(CAT_foursquares_type type, CAT_ivec2 position)
 		break;
 
 		case CAT_FOURSQUARES_O:
-			configure_collision_matrix
+			configure_collider
 			(
 				0, 1, 1, 0,
 				0, 1, 1, 0,
@@ -130,7 +185,7 @@ void spawn_piece(CAT_foursquares_type type, CAT_ivec2 position)
 		break;
 
 		case CAT_FOURSQUARES_S:
-			configure_collision_matrix
+			configure_collider
 			(
 				0, 1, 1, 0,
 				1, 1, 0, 0,
@@ -141,7 +196,7 @@ void spawn_piece(CAT_foursquares_type type, CAT_ivec2 position)
 		break;
 
 		case CAT_FOURSQUARES_T:
-			configure_collision_matrix
+			configure_collider
 			(
 				0, 1, 0, 0,
 				1, 1, 1, 0,
@@ -152,7 +207,7 @@ void spawn_piece(CAT_foursquares_type type, CAT_ivec2 position)
 		break;
 
 		case CAT_FOURSQUARES_Z:
-			configure_collision_matrix
+			configure_collider
 			(
 				1, 1, 0, 0,
 				0, 1, 1, 0,
@@ -164,94 +219,124 @@ void spawn_piece(CAT_foursquares_type type, CAT_ivec2 position)
 	}
 }
 
-void rotate_piece(CAT_foursquares_rotation rotation)
-{	
-	if(piece.type == CAT_FOURSQUARES_O)
-		return;
-
-	for(int y = 0; y < collision_h; y++)
+void reset_buffers()
+{
+	piece_position_buffer = piece_position;
+	piece_orientation_buffer = piece_orientation;
+	for(int y = 0; y < collider_h; y++)
 	{
-		for(int x = 0; x < collision_w; x++)
+		for(int x = 0; x < collider_w; x++)
 		{
-			if(rotation == CAT_FOURSQUARES_CW)
-				rotation_buffer_matrix[x][collision_h - y - 1] = collision_matrix[y][x];
-			else
-				rotation_buffer_matrix[y][x] = collision_matrix[x][collision_h - y - 1];
-		}
-	}
-
-	for(int y = 0; y < collision_h; y++)
-	{
-		for(int x = 0; x < collision_w; x++)
-		{
-			collision_matrix[y][x] = rotation_buffer_matrix[y][x];
+			collider_buffer[y][x] = collider[y][x];
 		}
 	}
 }
 
 void move_piece(int dx, int dy)
 {
-	piece.position.x += dx;
-	piece.position.y += dy;
+	piece_position_buffer.x += dx;
+	piece_position_buffer.y += dy;
 }
 
-void kill_piece()
-{
-	for(int dy = 0; dy < collision_h; dy++)
-	{
-		for(int dx = 0; dx < collision_w; dx++)
-		{
-			if(collision_matrix[dy][dx])
-			{
-				int x = piece.position.x + dx;
-				int y = piece.position.y + dy;
+void rotate_piece(CAT_foursquares_rotation rotation)
+{	
+	int rotation_offset = rotation == CAT_FOURSQUARES_CW ? 1 : -1;
+	piece_orientation_buffer = (piece_orientation + rotation_offset) % 4;
 
-				cells[y][x].full = true;
-				cells[y][x].colour = piece_colours[piece.type];
-			}
+	if(piece_type == CAT_FOURSQUARES_O)
+		return;
+
+	for(int y = 0; y < collider_h; y++)
+	{
+		for(int x = 0; x < collider_w; x++)
+		{
+			if(rotation == CAT_FOURSQUARES_CW)
+				collider_buffer[x][collider_h - y - 1] = collider[y][x];
+			else
+				collider_buffer[y][x] = collider[x][collider_h - y - 1];
 		}
 	}
-
-	spawn_piece
-	(
-		(piece.type + 1) % 7,
-		(CAT_ivec2) {CAT_rand_int(0, CAT_FOURSQUARES_GRID_WIDTH-collision_w), 0}
-	);
 }
 
-static int frame_counter = 0;
-bool should_trigger_step()
+bool validate_buffers()
 {
-	if(frame_counter == 4)
-	{
-		frame_counter = 0;
-		return true;
-	}
-	frame_counter += 1;
-	return false;
-}
-
-bool detect_overlap()
-{
-	for(int dy = 0; dy < collision_h; dy++)
+	for(int dy = 0; dy < collider_h; dy++)
 	{	
-		for(int dx = 0; dx < collision_w; dx++)
+		for(int dx = 0; dx < collider_w; dx++)
 		{
-			if(collision_matrix[dy][dx])
+			if(collider_buffer[dy][dx])
 			{
-				int x = piece.position.x + dx;
-				int y = piece.position.y + dy;
+				int x = piece_position_buffer.x + dx;
+				int y = piece_position_buffer.y + dy;
 
 				if(y >= CAT_FOURSQUARES_GRID_HEIGHT)
-					return true;
+					return false;
 				
 				if(x < 0)
-					return true;
+					return false;
 				if(x >= CAT_FOURSQUARES_GRID_WIDTH)
-					return true;
+					return false;
 
 				if(cells[y][x].full)
+					return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+void commit_buffers()
+{
+	piece_position = piece_position_buffer;
+	piece_orientation = piece_orientation_buffer;
+	for(int y = 0; y < collider_h; y++)
+	{
+		for(int x = 0; x < collider_w; x++)
+		{
+			collider[y][x] = collider_buffer[y][x];
+		}
+	}
+}
+
+bool enumerate_kicks(CAT_foursquares_rotation rotation)
+{
+	int kick_case = piece_orientation;
+	int kick_dir = rotation == CAT_FOURSQUARES_CW ? 1 : -1;
+
+	for(int i = 0; i < 5; i++)
+	{
+		switch (piece_type)
+		{
+			case CAT_FOURSQUARES_J:
+			case CAT_FOURSQUARES_L:
+			case CAT_FOURSQUARES_S:
+			case CAT_FOURSQUARES_T:
+			case CAT_FOURSQUARES_Z:
+			{
+				CAT_ivec2 base = jlstz_kick_offset_bases[i];
+				CAT_ivec2 signs = jlstz_kick_offset_signs[kick_case];
+				CAT_ivec2 offset = {base.x * signs.x, base.y * signs.y};
+				offset = CAT_ivec2_mul(offset, kick_dir);
+				piece_position_buffer = CAT_ivec2_add(piece_position, offset);
+				if(validate_buffers())
 					return true;
+				break;
+			}
+			
+			case CAT_FOURSQUARES_I:
+			{
+				CAT_ivec2 offset = i_kick_offsets[i][kick_case];
+				offset = CAT_ivec2_mul(offset, kick_dir);
+				piece_position_buffer = CAT_ivec2_add(piece_position, offset);
+				if(validate_buffers())
+					return true;
+				break;
+			}
+		
+			default:
+			{
+				return true;
 			}
 		}
 	}
@@ -259,16 +344,16 @@ bool detect_overlap()
 	return false;
 }
 
-bool detect_settle()
+bool is_piece_settled()
 {
-	for(int dy = 0; dy < collision_h; dy++)
+	for(int dy = 0; dy < collider_h; dy++)
 	{
-		for(int dx = 0; dx < collision_w; dx++)
+		for(int dx = 0; dx < collider_w; dx++)
 		{
-			if(collision_matrix[dy][dx])
+			if(collider[dy][dx])
 			{
-				int x = piece.position.x + dx;
-				int y = piece.position.y + dy;
+				int x = piece_position.x + dx;
+				int y = piece_position.y + dy;
 
 				if(y < CAT_FOURSQUARES_GRID_HEIGHT-1)
 				{
@@ -282,6 +367,24 @@ bool detect_settle()
 	}
 
 	return false;
+}
+
+void kill_piece()
+{
+	for(int dy = 0; dy < collider_h; dy++)
+	{
+		for(int dx = 0; dx < collider_w; dx++)
+		{
+			if(collider[dy][dx])
+			{
+				int x = piece_position.x + dx;
+				int y = piece_position.y + dy;
+
+				cells[y][x].full = true;
+				cells[y][x].colour = piece_colours[piece_type];
+			}
+		}
+	}
 }
 
 void clean_grid()
@@ -332,6 +435,18 @@ void clean_grid()
 	}
 }
 
+static int frame_counter = 0;
+bool should_trigger_step()
+{
+	if(frame_counter == 4)
+	{
+		frame_counter = 0;
+		return true;
+	}
+	frame_counter += 1;
+	return false;
+}
+
 void CAT_MS_foursquares(CAT_machine_signal signal)
 {
 	switch(signal)
@@ -353,7 +468,7 @@ void CAT_MS_foursquares(CAT_machine_signal signal)
 		case CAT_MACHINE_SIGNAL_TICK:
 		{
 			static bool quit = false;
-			if(CAT_input_pressed(CAT_BUTTON_B) || CAT_input_pressed(CAT_BUTTON_START))
+			if(CAT_input_pressed(CAT_BUTTON_START))
 				CAT_gui_open_popup("Quit Foursquares?\n\nProgress will not\nbe saved!\n", &quit);
 			if(quit)
 			{
@@ -363,45 +478,50 @@ void CAT_MS_foursquares(CAT_machine_signal signal)
 			if(CAT_gui_popup_is_open())
 				break;
 
-			if(CAT_input_pressed(CAT_BUTTON_A))
+			reset_buffers();
+
+			if(CAT_input_pressed(CAT_BUTTON_SELECT))
 			{
+				spawn_piece((piece_type+1)%7, (CAT_ivec2){0, 0});
+				reset_buffers();
+			}
+
+			if(CAT_input_pressed(CAT_BUTTON_A))
+			{	
 				rotate_piece(CAT_FOURSQUARES_CW);
-				if(detect_overlap())
-					rotate_piece(CAT_FOURSQUARES_CCW);
+				if(validate_buffers() || enumerate_kicks(CAT_FOURSQUARES_CW))
+					commit_buffers();
+			}
+			if(CAT_input_pressed(CAT_BUTTON_B))
+			{	
+				rotate_piece(CAT_FOURSQUARES_CCW);
+				if(validate_buffers() || enumerate_kicks(CAT_FOURSQUARES_CCW))
+					commit_buffers();
 			}
 
 			if(CAT_input_held(CAT_BUTTON_LEFT, 0))
-			{
 				move_piece(-1, 0);
-				if(detect_overlap())
-					move_piece(1, 0);
-			}		
 			if(CAT_input_held(CAT_BUTTON_RIGHT, 0))
-			{
 				move_piece(1, 0);
-				if(detect_overlap())
-					move_piece(-1, 0);
-			}
 			if(CAT_input_held(CAT_BUTTON_DOWN, 0))
-			{
 				move_piece(0, 1);
-				if(detect_overlap())
-					move_piece(0, -1);
-			}
+			if(validate_buffers())
+				commit_buffers();
 
 			if(should_trigger_step())
-			{
 				move_piece(0, 1);
-				if(detect_overlap())
-					move_piece(0, -1);
-			}
-
-			if(detect_settle())
+			if(validate_buffers())
+				commit_buffers();
+			else if(is_piece_settled())
 			{
-				CAT_printf("!\n");
 				kill_piece();
 				clean_grid();
-			}		
+				spawn_piece
+				(
+					(piece_type + 1) % 7,
+					(CAT_ivec2) {CAT_rand_int(0, CAT_FOURSQUARES_GRID_WIDTH-collider_w), 0}
+				);
+			}
 		break;
 		}
 
@@ -429,20 +549,20 @@ void CAT_render_foursquares()
 				);
 			}
 
-			for(int i = 0; i < collision_h; i++)
+			for(int i = 0; i < collider_h; i++)
 			{
-				int y_p = piece.position.y + i;
-				for(int j = 0; j < collision_w; j++)
+				int y_p = piece_position.y + i;
+				for(int j = 0; j < collider_w; j++)
 				{
-					int x_p = piece.position.x + j;
+					int x_p = piece_position.x + j;
 
-					if(collision_matrix[i][j])
+					if(collider[i][j])
 					{
 						CAT_fillberry
 						(
 							x_p * CAT_FOURSQUARES_TILE_SIZE, y_p * CAT_FOURSQUARES_TILE_SIZE,
 							CAT_FOURSQUARES_TILE_SIZE, CAT_FOURSQUARES_TILE_SIZE,
-							piece_colours[piece.type]
+							piece_colours[piece_type]
 						);
 					}
 				}
