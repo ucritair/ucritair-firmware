39b28ea03ee11b114c8fb4ce1da9868efa72c1d7
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2025-06-03 18:56:12 +0200

    0.3.23.81 Added first draft of dashboard

--- zephyrapp/game/src/monitors/cat_monitors.c
+++ zephyrapp/game/src/monitors/cat_monitors.c
@@ -66,11 +66,21 @@ static void render_page_markers()
 	}
 }
 
+static void render_exit_notice()
+{
+	const char* text = "[START] to exit";
+	int x = CAT_LCD_SCREEN_W - 6 - strlen(text) * CAT_GLYPH_WIDTH;
+	int y = CAT_LCD_SCREEN_H - 4 - CAT_GLYPH_HEIGHT;
+	CAT_push_text_colour(0x2455);
+	CAT_draw_text(x, y, text);
+}
+
 static void render_monitor()
 {
 	render_background();
 	render_page_markers();
 	routines[page].render();
+	// render_exit_notice();
 }
 
 void CAT_monitor_advance()
