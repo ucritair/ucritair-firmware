19671ede76ae0de9af0f7157f53ad50ec4bf449c
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2025-09-16 15:44:32 -0500

    Docs content

--- zephyrapp/game/utils/doc_gen/pages/gameplay/apartment.py
+++ zephyrapp/game/utils/doc_gen/pages/gameplay/apartment.py
@@ -12,7 +12,7 @@ def build(here, html: HTMLWriter):
 	
 	html.begin_text_block();
 	html.text("The apartment is where your critter lives.");
-	html.text("The critter can be interacted with in the apartment, and it can be decorated with a variety of items.");
+	html.text("Your critter can be interacted with in the apartment, and you can decorate your apartment with a variety of items.");
 	html.text("Additionally, it contains diegetic access points for the dashboard, the vending machine, and the arcade.");
 	html.text("The GUI overlaid on the apartment screen includes buttons to access the 3 care mechanics, decoration mode, and the menu.");
 	html.end_text_block();
@@ -20,7 +20,7 @@ def build(here, html: HTMLWriter):
 	html.newline();
 	
 	html.begin_text_block();
-	html.text("Generally speaking, the apartment is the \"bottom\" of the game's state machine. All roads lead back to the apartment.");
+	html.text("Generally speaking, the apartment is the root of the game's state machine. All roads lead back to the apartment.");
 	html.text("Exiting the dashboard will return you to the apartment, as will exiting the overworld.");
 	html.end_text_block();
 	
--- zephyrapp/game/utils/doc_gen/pages/gameplay/arcade.py
+++ zephyrapp/game/utils/doc_gen/pages/gameplay/arcade.py
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env python3
 
-# PAGE(Apartment)
+# PAGE(Arcade)
 
 from html_writer import HTMLWriter;
 import common;
@@ -11,4 +11,18 @@ def build(here, html: HTMLWriter):
 	common.index_banner(html);
 	common.title(html, here.title);
 
-	
+	html.text("The arcade offers minigames to be played for enjoyment and the occasional reward." \
+	"The current games are Snack, Sweep, and Foursquares");
+
+	html.heading(3, "Snack");
+	html.text("Maneuver Snackcat with the D-pad to snap up snacks and coins." \
+	"Beware! If you collide with the walls or yourself, it's game over.");
+
+	html.heading(3, "Sweep");
+	html.text("Carefully unearth a minefield tile-by-tile." \
+	"Tearing up a mine spells certain doom. The number on a tile indicates its distance in tiles from the nearest mine.");
+
+	html.heading(3, "Foursquares");
+	html.text("Stack up 4-squared geometry to form a continuous block." \
+	"A fully-formed block will dissolve and score you points." \
+	"If your geometry touches the ceiling, it's game over.");
