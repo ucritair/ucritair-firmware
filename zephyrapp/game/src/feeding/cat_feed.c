0dcd074abfba95b49e180b93ee0955ea99235422
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2025-10-25 16:11:26 -0500

    1.7.47.82 safer food positioning

--- zephyrapp/game/src/feeding/cat_feed.c
+++ zephyrapp/game/src/feeding/cat_feed.c
@@ -847,6 +847,12 @@ static void MS_arrange(CAT_FSM_signal signal)
 						food_list[0].position.y - ty,
 					};
 				}
+
+				for(int i = 0; i < food_count; i++)
+				{
+					food_list[i].position.x = clamp(food_list[i].position.x, FOOD_COLLISION_R, CAT_LCD_SCREEN_W-FOOD_COLLISION_R);
+					food_list[i].position.y = clamp(food_list[i].position.y, FOOD_COLLISION_R, CAT_LCD_SCREEN_H-FOOD_COLLISION_R);
+				}
 			}
 			else if (CAT_input_touch_up())
 			{
