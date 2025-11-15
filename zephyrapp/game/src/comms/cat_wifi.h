9c076089821e2491cfb5b4019a0f516d1c48abd7
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2025-11-15 17:00:40 -0600

    WIPgit add -A

--- zephyrapp/game/src/comms/cat_wifi.h
+++ zephyrapp/game/src/comms/cat_wifi.h
@@ -10,9 +10,11 @@ typedef enum
 	CAT_WIFI_CONNECTION_FAILURE
 } CAT_wifi_connection_status;
 
-void CAT_MS_wifi(CAT_FSM_signal signal);
-void CAT_draw_wifi();
-
+bool CAT_wifi_init();
+bool CAT_wifi_connect(const char* ssid, const char* password, uint8_t auth_mode, uint32_t timeout_ms);
 void CAT_wifi_autoconnect(int timeout_ms);
 bool CAT_is_wifi_connected();
 char* CAT_get_wifi_SSID();
+
+void CAT_MS_wifi(CAT_FSM_signal signal);
+void CAT_draw_wifi();
